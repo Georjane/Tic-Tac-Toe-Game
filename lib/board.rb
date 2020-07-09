4ea7abd02468c0e2c9c1289ea62693dc80d0ae11
Georjane <60772732+Georjane@users.noreply.github.com>
2020-07-09 13:45:41 +0100

    Add display_board and swap methods

--- lib/board.rb
+++ lib/board.rb
@@ -11,4 +11,16 @@ class Board < Player
     @wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
   end
 
+  def display_board
+    "\n #{board[0]} | #{board[1]} | #{board[2]} \n ---------\n #{board[3]} | #{board[4]} | #{board[5]} \n ---------\n #{board[6]} | #{board[7]} | #{board[8]} \n\n"
+  end
+
+  def swap(num)
+    if @counts.odd?
+    board[num - 1] = player1.piece_x
+    else
+    board[num - 1] = player2.piece_o
+    end
+  end
+
 end
